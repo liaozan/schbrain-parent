d4661932dbaa61e61c59fe63b0b14b93513d4618
liaozan <378024053@qq.com>
2023-07-17 16:02:49 +0800

    Introduce MessageProducer in kafka-starter

--- starters/kafka-spring-boot-starter/src/main/java/com/schbrain/framework/autoconfigure/kafka/KafkaAutoConfiguration.java
+++ starters/kafka-spring-boot-starter/src/main/java/com/schbrain/framework/autoconfigure/kafka/KafkaAutoConfiguration.java
@@ -3,12 +3,14 @@ package com.schbrain.framework.autoconfigure.kafka;
 import com.schbrain.framework.autoconfigure.kafka.properties.KafkaProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author liaozan
  * @since 2023-04-29
  */
 @AutoConfiguration
+@Import(MessageProducer.class)
 @EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaAutoConfiguration {
 

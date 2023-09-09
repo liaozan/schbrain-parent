f8f0509946b80e4070128b57d01b1227c9666040
liaozan <378024053@qq.com>
2023-09-10 03:10:41 +0800

    Refactor AES128ECBWithPKCS7
    Before this commit, The AES128ECBWithPKCS7 is encode/decode text to base64 default
    Overloaded a method to support whether to convert with base64

--- commons/common-util/src/main/java/com/schbrain/common/util/cipher/AES128ECBWithPKCS7.java
+++ commons/common-util/src/main/java/com/schbrain/common/util/cipher/AES128ECBWithPKCS7.java
@@ -1,7 +1,9 @@
 package com.schbrain.common.util.cipher;
 
 import cn.hutool.crypto.symmetric.AES;
+import com.schbrain.common.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
@@ -19,40 +21,55 @@ public class AES128ECBWithPKCS7 {
     /**
      * encrypt input text
      */
-    public static String encrypt(String input, String key) {
-        byte[] encrypted = encryptByte(input, key);
-        if (null == encrypted) {
-            return "";
-        }
-        return Base64.getEncoder().encodeToString(encrypted);
+    public static String encrypt(String input, String secretKey) {
+        return encrypt(input, secretKey, true);
     }
 
-    public static byte[] encryptByte(String input, String key) {
-        byte[] encrypted;
-        try {
-            encrypted = new AES("ECB", "PKCS7Padding", key.getBytes(UTF_8)).encrypt(input);
-        } catch (Exception e) {
-            log.error("AES128ECBWithPKCS7 encrypt ({}) error! ", input, e);
-            return null;
-        }
-        return encrypted;
+    /**
+     * encrypt input text
+     */
+    public static String encrypt(String input, String secretKey, boolean toBase64) {
+        byte[] encrypted = encryptByte(input.getBytes(UTF_8), secretKey);
+        return toBase64 ? Base64.getEncoder().encodeToString(encrypted) : new String(encrypted, UTF_8);
     }
 
     /**
      * decrypt input text
      */
-    public static String decrypt(String input, String key) {
-        return decryptByte(Base64.getDecoder().decode(input), key);
+    public static String decrypt(String input, String secretKey) {
+        return decrypt(input, secretKey, true);
     }
 
-    public static String decryptByte(byte[] input, String key) {
-        try {
-            byte[] output = new AES("ECB", "PKCS7Padding", key.getBytes(UTF_8)).decrypt(input);
-            return new String(output, UTF_8);
-        } catch (Exception e) {
-            log.error("AES128ECBWithPKCS7 decryptByte ({}) error!", new String(input, UTF_8), e);
-            return "";
+    /**
+     * decrypt input text
+     */
+    public static String decrypt(String input, String secretKey, boolean isBase64) {
+        byte[] bytes = isBase64 ? Base64.getDecoder().decode(input) : input.getBytes(UTF_8);
+        return new String(decryptByte(bytes, secretKey), UTF_8);
+    }
+
+    /**
+     * encrypt input bytes
+     */
+    public static byte[] encryptByte(byte[] input, String secretKey) {
+        return createAes(secretKey).encrypt(input);
+    }
+
+    /**
+     * decrypt input bytes
+     */
+    public static byte[] decryptByte(byte[] input, String secretKey) {
+        return createAes(secretKey).decrypt(input);
+    }
+
+    /**
+     * create aes crypto
+     */
+    private static AES createAes(String secretKey) {
+        if (StringUtils.isBlank(secretKey)) {
+            throw new BaseException("secretKey is blank!");
         }
+        return new AES("ECB", "PKCS7Padding", secretKey.getBytes(UTF_8));
     }
 
 }

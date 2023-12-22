9b1c830820dc7cb4a812efff4631c20b75fa1e1d
liaozan <378024053@qq.com>
2023-12-22 11:59:45 +0800

    Add utils for canalEvent convert

---
+++ starters/starrocks-spring-boot-starter/src/main/java/com/schbrain/framework/autoconfigure/starrocks/helper/ConvertUtils.java
@@ -0,0 +1,34 @@
+package com.schbrain.framework.autoconfigure.starrocks.helper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.schbrain.common.entity.CanalChangedEvent;
+import com.schbrain.common.exception.BaseException;
+import com.schbrain.common.util.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.util.List;
+
+/**
+ * @author liaozan
+ * @since 2023/12/22
+ */
+public class ConvertUtils {
+
+    private static final ObjectMapper DESERIALIZER = JacksonUtils.getObjectMapper().copy().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
+    public static <Source, Target> Target convertTo(ConsumerRecord<String, String> record, Class<Source> sourceType, Class<Target> targetType) {
+        CanalChangedEvent event = JacksonUtils.getObjectFromJson(record.value(), CanalChangedEvent.class);
+        if (event == null) {
+            throw new BaseException("CanalChangedEvent is null");
+        }
+        Source source = DESERIALIZER.convertValue(event.getAfter(), sourceType);
+        return BeanCopyUtils.copy(source, targetType);
+    }
+
+    public static <Source, Target> List<Target> convertToList(ConsumerRecords<String, String> records, Class<Source> sourceType, Class<Target> targetType) {
+        return StreamUtils.toList(records, record -> convertTo(record, sourceType, targetType));
+    }
+
+}
